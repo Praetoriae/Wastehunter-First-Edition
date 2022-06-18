564e9edd7e9c23dde976fbc32c11a58cf4b8e4c6
Praetoriae <107775699+Praetoriae@users.noreply.github.com>
2022-06-19 00:26:30 +0100

    Add files via upload

---
+++ Wastehunter/module/helpers/config.mjs
@@ -0,0 +1,29 @@
+export const WASTEHUNTER = {};
+
+// Define constants here, such as:
+WASTEHUNTER.foobar = {
+  'bas': 'WASTEHUNTER.bas',
+  'bar': 'WASTEHUNTER.bar'
+};
+
+/**
+ * The set of Ability Scores used within the sytem.
+ * @type {Object}
+ */
+ WASTEHUNTER.abilities = {
+  "str": "WASTEHUNTER.AbilityStr",
+  "dex": "WASTEHUNTER.AbilityDex",
+  "con": "WASTEHUNTER.AbilityCon",
+  "int": "WASTEHUNTER.AbilityInt",
+  "wis": "WASTEHUNTER.AbilityWis",
+  "cha": "WASTEHUNTER.AbilityCha"
+};
+
+WASTEHUNTER.abilityAbbreviations = {
+  "str": "WASTEHUNTER.AbilityStrAbbr",
+  "dex": "WASTEHUNTER.AbilityDexAbbr",
+  "con": "WASTEHUNTER.AbilityConAbbr",
+  "int": "WASTEHUNTER.AbilityIntAbbr",
+  "wis": "WASTEHUNTER.AbilityWisAbbr",
+  "cha": "WASTEHUNTER.AbilityChaAbbr"
+};
